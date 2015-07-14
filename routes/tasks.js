83ac5c2dad8200c2acc1112113fe5306ab43f280
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-14 01:56:41 -0500

    Add new task function

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,3 +9,20 @@ exports.list = function (req, res, next) {
     });
   });
 };
+
+exports.add = function (req, res, next) {
+  if (!req.body || !req.body.name) {
+    return next(new Error('No data provided'));
+  }
+  req.db.tasks.save({
+    name: req.body.name,
+    completed: false
+  }, function (err, task) {
+    if (err) return next(err);
+    if (!task) return next(new Error('Failed to save'));
+
+    console.info('Added %s with id=%s', task.name, task._id);
+
+    res.redirect('/tasks');
+  });
+};
