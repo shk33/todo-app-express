4332f9653c19524fc00a5da022767531b1c915f6
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-14 01:41:06 -0500

    Create server

--- app.js
+++ app.js
@@ -68,3 +68,7 @@ app.all('*', function (req, res) {
 if ('development' == app.get('env')) {
   app.use(errorhandler());
 }
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port'+ app.get('port'));
+});
